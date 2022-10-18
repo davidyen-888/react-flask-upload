22b62a07727272700437585f2275d3248af18099
David Hsieh <yen888yen888@gmail.com>
2022-10-17 21:33:58 -0700

    Add README, add selected file name before upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# React-Flask-Upload
+
+This is a simple example of how to upload files by using React and Flask.
+
+## Installation
+
+### Server
+
+1. Create a virtual environment and activate it using `python3 -m venv env` and `source env/bin/activate`
+2. Upgrade pip to the latest version using `pip install --upgrade pip`
+3. Install flask using `pip install flask`
+4. Run the server using `flask run` in the virtual environment
+
+### Client
+
+1. Install the dependencies using `npm install`
+2. Run the client using `npm start`
+
+## Usage
+
+1. Select a file from your computer
+2. Click the upload button
+3. The file will be uploaded and saved in the local directory `backend/app/Downloads`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,24 @@ import "./App.css";
 
 function App() {
   const [fileURL, setfileURL] = useState("");
+  const [selectedFile, setselectedFile] = useState(null);
 
   let uploadInput = React.createRef();
+
+  const handleSelectFile = (e) => {
+    const data = [];
+    for (let i = 0; i < e.target.files.length; i++) {
+      data.push(e.target.files.item(i));
+    }
+    setselectedFile(data);
+  };
 
   const handleUploadFile = (ev) => {
     ev.preventDefault();
 
     const data = new FormData();
     for (let i = 0; i < uploadInput.files.length; i++) {
-      data.append("file", uploadInput.files[i]);
+      data.append("file", uploadInput.files[i], uploadInput.files[i].name);
       fetch("http://localhost:5000/upload", {
         method: "POST",
         body: data,
@@ -28,6 +37,7 @@ function App() {
   return (
     <>
       <div className="container">
+        <h1>React-Flask File Upload</h1>
         <h3>
           This is a simple file upload app, upload your files and you can see
           the saved files in the backend/app/Downloads folder
@@ -40,7 +50,15 @@ function App() {
             ref={(ref) => {
               uploadInput = ref;
             }}
+            onChange={handleSelectFile}
           />
+          <div className="selectFile">
+            <h3>Selected file(s): </h3>
+            {selectedFile &&
+              selectedFile.map((item, index) => {
+                return <div key={index}>{item.name}</div>;
+              })}
+          </div>
           <button className="formButton" type="submit">
             Upload
           </button>
